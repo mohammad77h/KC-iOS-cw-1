200d9bf960c8ae4466d59d5906a81707c80e92a2
mohammad <mphammadkiller939@gmail.com>
2022-10-05 09:14:19 -0700

    Project app

---
+++ 999/999/_99App.swift
@@ -0,0 +1,17 @@
+//
+//  _99App.swift
+//  999
+//
+//  Created by Mac on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct _99App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
